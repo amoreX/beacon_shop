62babeac5a6c418cac6c9c07efdcc3667b7c8bd5
WahidX <wahidx93@gmail.com>
2024-05-10 14:15:06 +0530

    animation-done-mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import Title from "./Components/Landing/titlepage.jsx";
-import Birthday from "./Components/Birthday/Birthday.jsx"
-import "./app.scss"
-
+import Birthday from "./Components/Birthday/Birthday.jsx";
+import "./app.scss";
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
+import { useEffect, useState } from "react";
 function App() {
+	const [lastScrollTop, setLastScrollTop] = useState(0);
+
+	const scrollBy100vh = (event) => {
+		const currentScrollTop = window.scrollY;
+		const delta = currentScrollTop - lastScrollTop; // positive for down, negative for up
+
+		if (delta > 0) {
+			// Scrolled down, scroll down by 100vh
+			window.scrollBy(0, window.innerHeight);
+		} else if (delta < 0) {
+			// Scrolled up, scroll up by 100vh
+			window.scrollBy(0, -window.innerHeight);
+		}
+
+		setLastScrollTop(currentScrollTop);
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", scrollBy100vh);
+
+		// Cleanup function to remove listener on unmount
+		return () => window.removeEventListener("scroll", scrollBy100vh);
+	}, []);
 	return (
 		<>
-			<div id="body-container">
-			<Title />
-			<Birthday />
-			</div>
+			<ReactLenis root>
+				<div id="body-container">
+					<Title />
+					<Birthday />
+				</div>
+			</ReactLenis>
 		</>
 	);
 }
