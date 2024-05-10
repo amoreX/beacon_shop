f6f4b29a0814c527e3864f3e642a099f46be8cb8
WahidX <wahidx93@gmail.com>
2024-05-10 14:19:03 +0530

    animation-done-birthday

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,44 @@ import "./app.scss";
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import { useEffect, useState } from "react";
 function App() {
-	const [lastScrollTop, setLastScrollTop] = useState(0);
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	// const [lastScrollTop, setLastScrollTop] = useState(0);
+	// const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setWindowWidth(window.innerWidth);
-		};
+	// useEffect(() => {
+	// 	const handleResize = () => {
+	// 		setWindowWidth(window.innerWidth);
+	// 	};
 
-		window.addEventListener("resize", handleResize);
+	// 	window.addEventListener("resize", handleResize);
 
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
-	const scrollBy100vh = (event) => {
-		const currentScrollTop = window.scrollY;
-		const delta = currentScrollTop - lastScrollTop; // positive for down, negative for up
-		if (windowWidth > 1100) {
-			if (delta > 0) {
-				// Scrolled down, scroll down by 100vh
-				window.scrollBy(0, window.innerHeight);
-			} else if (delta < 0) {
-				// Scrolled up, scroll up by 100vh
-				window.scrollBy(0, -window.innerHeight);
-			}
+	// 	return () => {
+	// 		window.removeEventListener("resize", handleResize);
+	// 	};
+	// }, []);
+	// const scrollBy100vh = (event) => {
+	// 	const currentScrollTop = window.scrollY;
+	// 	const delta = currentScrollTop - lastScrollTop; // positive for down, negative for up
+	// 	if (windowWidth > 1100) {
+	// 		if (delta > 0) {
+	// 			// Scrolled down, scroll down by 100vh
+	// 			window.scrollBy(0, window.innerHeight);
+	// 		} else if (delta < 0) {
+	// 			// Scrolled up, scroll up by 100vh
+	// 			window.scrollBy(0, -window.innerHeight);
+	// 		}
 
-			setLastScrollTop(currentScrollTop);
-		}
-	};
+	// 		setLastScrollTop(currentScrollTop);
+	// 	}
+	// };
 
-	useEffect(() => {
-		if (windowWidth > 1100) {
-			window.addEventListener("scroll", scrollBy100vh);
-		}
+	// useEffect(() => {
+	// 	if (windowWidth > 1100) {
+	// 		window.addEventListener("scroll", scrollBy100vh);
+	// 	}
 
-		// Cleanup function to remove listener on unmount
-		return () => window.removeEventListener("scroll", scrollBy100vh);
-	}, []);
+	// 	// Cleanup function to remove listener on unmount
+	// 	return () => window.removeEventListener("scroll", scrollBy100vh);
+	// }, []);
 	return (
 		<>
 			<ReactLenis root>
