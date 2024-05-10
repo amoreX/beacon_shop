5a3f6a92b240afd9ea0bba4b2610c58f4b1c5aba
WahidX <wahidx93@gmail.com>
2024-05-10 14:03:33 +0530

    animation-done-mobile

--- src/Components/Birthday/Birthday.jsx
+++ src/Components/Birthday/Birthday.jsx
@@ -4,6 +4,19 @@ import "./Birthday.scss";
 
 export default function Birthday() {
 	const [mark, setMark] = useState(0);
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	const [pics, setPics] = useState([
 		{ top: "4%", left: "4%", width: "24%", height: "60%" }, // pic1
 		{ top: "4%", left: "32%", width: "24%", height: "32%" }, // pic2
@@ -60,11 +73,43 @@ export default function Birthday() {
 		{ top: "68%", left: "4%", width: "36%", height: "28%" }, // pic5
 		{ top: "68%", left: "44%", width: "24%", height: "28%" }, // pic6
 	]);
-	const listofpics = [pics1, pics2, pics3, pics4, pics5, pics6];
+	const [picsM, setPicsM] = useState([
+		{ top: "4%", left: "4%", width: "42%", height: "40%" }, // pic1
+		{ top: "4%", left: "52%", width: "40%", height: "18%" }, // pic2
+		{ top: "26%", left: "52%", width: "40%", height: "18%" }, // pic3
+		{ top: "56%", left: "4%", width: "42%", height: "18%" }, // pic4
+		{ top: "78%", left: "4%", width: "42%", height: "18%" }, // pic5
+		{ top: "56%", left: "52%", width: "42%", height: "40%" }, // pic6
+	]);
+	const [pics2M, setPics2M] = useState([
+		{ top: "26%", left: "52%", width: "40%", height: "18%" }, // pic1
+		{ top: "4%", left: "4%", width: "42%", height: "40%" }, // pic2
+		{ top: "4%", left: "52%", width: "40%", height: "18%" }, // pic3
+		{ top: "78%", left: "4%", width: "42%", height: "18%" }, // pic4
+		{ top: "56%", left: "52%", width: "42%", height: "40%" }, // pic5
+		{ top: "56%", left: "4%", width: "42%", height: "18%" }, // pic6
+	]);
+	const [pics3M, setPics3M] = useState([
+		{ top: "4%", left: "52%", width: "40%", height: "18%" }, // pic1
+		{ top: "26%", left: "52%", width: "40%", height: "18%" }, // pic2
+		{ top: "4%", left: "4%", width: "42%", height: "40%" }, // pic3
+		{ top: "56%", left: "52%", width: "42%", height: "40%" }, // pic4
+		{ top: "56%", left: "4%", width: "42%", height: "18%" }, // pic5
+		{ top: "78%", left: "4%", width: "42%", height: "18%" }, // pic6
+	]);
+	let listofpics = [pics1, pics2, pics3, pics4, pics5, pics6];
+
+	if (windowWidth <= 1100) {
+		listofpics = [picsM, pics2M, pics3M];
+	}
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
-			setMark((prev) => (prev + 1) % 6);
+			if (windowWidth <= 1100) {
+				setMark((prev) => (prev + 1) % 3);
+			} else {
+				setMark((prev) => (prev + 1) % 6);
+			}
 		}, 2500); // Adjust the interval duration as per your preference
 
 		return () => clearInterval(intervalId);
@@ -80,6 +125,7 @@ export default function Birthday() {
 				{pics.map((pic, index) => {
 					return (
 						<div
+							key={index}
 							id="pic"
 							style={{
 								top: pics[index]["top"],
---
+++ src/Components/Birthday/mobiletemp.js
@@ -0,0 +1,25 @@
+const [picsM, setPicsM] = useState([
+	{ top: "4%", left: "4%", width: "42%", height: "40%" }, // pic1
+	{ top: "4%", left: "52%", width: "40%", height: "18%" }, // pic2
+	{ top: "26%", left: "52%", width: "40%", height: "18%" }, // pic3
+	{ top: "56%", left: "4%", width: "42%", height: "18%" }, // pic4
+	{ top: "78%", left: "4%", width: "42%", height: "18%" }, // pic5
+	{ top: "56%", left: "52%", width: "42%", height: "40%" }, // pic6
+]);
+const [pics2M, setPics2M] = useState([
+	{ top: "26%", left: "52%", width: "40%", height: "18%" }, // pic1
+	{ top: "4%", left: "4%", width: "42%", height: "40%" }, // pic2
+	{ top: "4%", left: "52%", width: "40%", height: "18%" }, // pic3
+	{ top: "78%", left: "4%", width: "42%", height: "18%" }, // pic4
+	{ top: "56%", left: "52%", width: "42%", height: "40%" }, // pic5
+	{ top: "56%", left: "4%", width: "42%", height: "18%" }, // pic6
+]);
+
+const [pics3M, setPics3M] = useState([
+	{ top: "4%", left: "52%", width: "40%", height: "18%" }, // pic1
+	{ top: "26%", left: "52%", width: "40%", height: "18%" }, // pic2
+	{ top: "4%", left: "4%", width: "42%", height: "40%" }, // pic3
+	{ top: "56%", left: "52%", width: "42%", height: "40%" }, // pic4
+	{ top: "56%", left: "4%", width: "42%", height: "18%" }, // pic5
+	{ top: "78%", left: "4%", width: "42%", height: "18%" }, // pic6
+]);
