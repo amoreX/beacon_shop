56d4317f4d79fd28521fa90381e4bf44acae3e1a
WahidX <wahidx93@gmail.com>
2024-05-13 17:59:23 +0530

    footer-done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,8 +12,42 @@ export default function Footer() {
 				</span>
 			</div>
 			<div id="footer-content">
-				<div id="left-content"></div>
-				<div id="right-content"></div>
+				<div id="left-content">
+					<div id="title-footer-left">Reach Us Directly</div>
+					<div id="content-footer-left">
+						<div id="kk">
+							<div>Organisers Name</div>
+							<div>Address</div>
+							<div>City,State,Zip Code</div>
+							<div>
+								<span style={{ fontWeight: "600" }}>Phone:</span>[Phone Number]
+							</div>
+							<div>
+								<span style={{ fontWeight: "600" }}>Email:</span>[Email Address]
+							</div>
+						</div>
+						<div style={{ fontWeight: "600" }}>Business Hours</div>
+						<div id="kk">
+							<div>Monday - Friday: [Opening Hours]</div>
+							<div>Saturday: [Opening Hours]</div>
+							<div>Sunday: [Opening Hours]</div>
+						</div>
+						<div style={{ fontWeight: "600" }}>Connect with Us</div>
+					</div>
+				</div>
+				<div id="bar"></div>
+				<div id="right-content">
+					<div id="title-footer-right">Get in Touch</div>
+					<div id="right-content-sub">Have questions or ready to start planning your event? </div>
+					<div id="right-content-sub">Fill out the form below. </div>
+					<div id="email-box">
+						<textarea placeholder="Email Address"></textarea>
+					</div>
+					<div id="content-email">
+						<textarea placeholder="Please use this space to provide details about your event such as date, location, estimated guests, theme preferences, or any specific requests you may have. "></textarea>
+					</div>
+					<div id="send-btn">Send</div>
+				</div>
 			</div>
 		</div>
 	);
